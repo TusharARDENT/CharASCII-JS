94fafa0e4943c7df1a1d397d489c2231530ed7c6
TusharArdent <tushar.kadam@ardentisys.com>
2024-11-29 01:13:14 +0530

    Added RegEx to the index.html for testing input

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,46 @@
 // Functionality of the page should appear here
-console.log("Hello ASCII")
+console.log("Hello ASCII");
 
 // const output = document.getElementsByClassName("output");
 // const input = document.getElementsByClassName("charInput");
 
 console.log(document.getElementsByClassName(""));
 
-function keyPress(event){
-    console.log(event.key)
-    let key = event.key;
-    let value = key.charCodeAt(0)
-    console.log(value)
-    document.querySelector(".output").textContent = `You have entered ${key} and it's ASCII value is ${value}`;
+function keyPress(event) {
+  console.log(event.key);
+  let key = event.key;
+  let value = key.charCodeAt(0);
+  console.log(value);
+  const valid = isValid(key);
+  document.querySelector(
+    ".output"
+  ).textContent = `You have entered ${key} and it's ASCII value is ${value}`;
+  document.querySelector(".charInput").addEventListener("keydown", keyDown);
 
-    document.querySelector(".charInput").addEventListener("keydown", function(event){
-        let newKey = event.key;
-        console.log("keyUp")
-        document.querySelectorAll(".charInput").value = newkey;
-    })
+  if (key.length > 1) {
+    return false;
+  }
+
+  console.log(isValid(key));
+  if (valid) {
+    console.log("valid");
+  } else if (!valid) {
+    console.log("Invalid");
+    document.querySelector(
+      ".output"
+    ).textContent = `You have entered ${key} and It is not a valid input`;
+    document.getElementsByTagName("body").classList.addClass("");
+  }
 }
 
+function keyDown(event) {
+  let newKey = event.key;
+  document.querySelector(".charInput").value = newKey;
+}
 
-// console.log(event.key)
-// let key = event.key;
-// let value = key.charCodeAt(0)
-// console.log(value)
-// document.querySelector(".output").textContent = `You have entered ${key} and it's ASCII value is ${value}`;
-
-// document.querySelector(".charInput").removeEventListener("keydown",keyDown)
-// document.querySelector(".charInput").addEventListener("keydown", keyDown);
-// }
-// function keyDown(event){
-// let newKey = event.key;
-// console.log("keyUp",newKey);
-// document.querySelector(".charInput").value = newKey;
-// }
+function isValid(val) {
+  const myRegEx = /^[A-Za-z0-9]/;
+  console.log(myRegEx);
+  const isValid = myRegEx.test(val);
+  return isValid;
+}
