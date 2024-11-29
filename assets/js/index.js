63e6fdcc8399adae90cfc6e23825dadc545f200a
TusharKadamARDENT <tushar.kadam@ardentisys.com>
2024-11-29 18:54:16 +0530

    Added event.target.value for the input value

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,35 @@
 // Functionality of the page should appear here
+
+const input = document.querySelector(".charInput");
+const output = document.querySelector(".output");
+const body = document.querySelector(".body");
+
+input.addEventListener("keydown", keyPress);
+
 function keyPress(event) {
     console.log(event.key);
     const key = event.key;
     let value = key.charCodeAt(0);
-    // console.log(value);
     const valid = isValid(key);
+
     if (valid) {
-        console.log("valid");     
-        document.querySelector(".output").textContent = `You have entered ${key} and it's ASCII value is ${value}`;
-        document.querySelector(".charInput").addEventListener("keydown", keyDown);        
-        // console.log(isValid(key));
-        document.querySelector(".body").classList.remove("error")
-        document.querySelector(".output").classList.remove("error-word");
-
+        console.log("valid");
+        output.textContent = `You have entered ${key} and it's ASCII value is ${value}`;
+        if (key.length === 1) {
+            event.target.value = key;
+        } else {
+            event.target.value = "";
+            output.textContent = `JS Output`;
+        }
+        body.classList.remove("error")
+        output.classList.remove("error-word");
     } else {
-        console.log("Invalid");    
-        document.querySelector(".charInput").addEventListener("keydown", keyDown);  
-        document.querySelector(".output").textContent = `You have not entered a valid input`;      
-        document.querySelector(".output").classList.add("error-word");
-        console.log(document.querySelector(".body").classList.add("error"));
+        console.log("Invalid");
+        output.textContent = `You have entered ${key} it is not a valid input`;
+        output.classList.add("error-word");
+        console.log(body.classList.add("error"));
     }
-}
-
-function keyDown(event) {
-    let newKey = event.key;
-    if(newKey.length === 1){
-        document.querySelector(".charInput").value = newKey;
-    }else{
-        document.querySelector(".charInput").value = "";
-        document.querySelector(".output").textContent = `JS Output`;
-    }
+    console.log(event.target.value)
 }
 
 function isValid(val) {
