98b4fb12e0aa205289b99c97154eda847af2ce04
TusharKadamARDENT <tushar.kadam@ardentisys.com>
2024-11-29 16:20:40 +0530

    Added Complete Functionality to the CharASCII finder in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,39 @@
 // Functionality of the page should appear here
-console.log("Hello ASCII");
-
-// const output = document.getElementsByClassName("output");
-// const input = document.getElementsByClassName("charInput");
-
-console.log(document.getElementsByClassName(""));
-
 function keyPress(event) {
-  console.log(event.key);
-  let key = event.key;
-  let value = key.charCodeAt(0);
-  console.log(value);
-  const valid = isValid(key);
-  document.querySelector(
-    ".output"
-  ).textContent = `You have entered ${key} and it's ASCII value is ${value}`;
-  document.querySelector(".charInput").addEventListener("keydown", keyDown);
+    console.log(event.key);
+    const key = event.key;
+    let value = key.charCodeAt(0);
+    // console.log(value);
+    const valid = isValid(key);
+    if (valid) {
+        console.log("valid");     
+        document.querySelector(".output").textContent = `You have entered ${key} and it's ASCII value is ${value}`;
+        document.querySelector(".charInput").addEventListener("keydown", keyDown);        
+        // console.log(isValid(key));
+        document.querySelector(".body").classList.remove("error")
+        document.querySelector(".output").classList.remove("error-word");
 
-  if (key.length > 1) {
-    return false;
-  }
-
-  console.log(isValid(key));
-  if (valid) {
-    console.log("valid");
-  } else if (!valid) {
-    console.log("Invalid");
-    document.querySelector(
-      ".output"
-    ).textContent = `You have entered ${key} and It is not a valid input`;
-    document.getElementsByTagName("body").classList.addClass("");
-  }
+    } else {
+        console.log("Invalid");    
+        document.querySelector(".charInput").addEventListener("keydown", keyDown);  
+        document.querySelector(".output").textContent = `You have not entered a valid input`;      
+        document.querySelector(".output").classList.add("error-word");
+        console.log(document.querySelector(".body").classList.add("error"));
+    }
 }
 
 function keyDown(event) {
-  let newKey = event.key;
-  document.querySelector(".charInput").value = newKey;
+    let newKey = event.key;
+    if(newKey.length === 1){
+        document.querySelector(".charInput").value = newKey;
+    }else{
+        document.querySelector(".charInput").value = "";
+        document.querySelector(".output").textContent = `JS Output`;
+    }
 }
 
 function isValid(val) {
-  const myRegEx = /^[A-Za-z0-9]/;
-  console.log(myRegEx);
-  const isValid = myRegEx.test(val);
-  return isValid;
+    const myRegEx = /^[A-Za-z0-9]/;
+    const isValid = myRegEx.test(val);
+    return isValid;
 }
